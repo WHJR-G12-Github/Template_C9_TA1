d4635c6ac6c67c178ee4b8ce8572ed2b18dffc85
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-11-01 17:53:23 +0530

    Create template_c9_ta1.py

---
+++ template_c9_ta1.py
@@ -0,0 +1,46 @@
+# Import 'pygame'
+
+
+# Initialize 'pygame'
+
+
+# Create a display screen of width 400 and height 600
+
+
+# Set a title for the game as 'Infinite Flying Bird Game'
+
+
+# Create an empty dictionary 'images'
+
+
+# Load the images into the dictionary
+
+
+
+
+# Create a rectangle and name it 'bird'
+
+
+# Create a 'game loop'
+
+  
+  # Display the background image at [0,0]
+  
+  # Display the ground image at [0,550]
+  
+  # Display the bird image over the rectangle 'bird'
+  
+  
+  # Handle the event to QUIT the game
+  # Create a for loop to list the events one by one
+  
+    # Check if the 'QUIT' button is pressed
+    
+        # Quit the game
+       
+        
+  # Update the display
+  
+  
+  # Mention the number of frames as 30 to create motion effect
+  
